681207c46227749286c71c860472b2ed5738f647
hmarszalek <marszalekhania5@gmail.com>
2026-01-28 05:08:12 +0100

    Introduce BridgedClusterState class

---
+++ src/Cassandra/RustBridge/BridgedClusterState.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.Runtime.CompilerServices;
+
+namespace Cassandra
+{
+    internal sealed class BridgedClusterState : RustResource
+    {
+        internal BridgedClusterState(ManuallyDestructible mdClusterState) : base(mdClusterState)
+        {
+        }
+
+        internal bool Equals(BridgedClusterState other)
+        {
+            if (other is null) return false;
+            return handle == other.handle;
+        }
+    }
+}
